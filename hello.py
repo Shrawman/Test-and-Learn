e7dbecd9f515b10a54efabbd7385d5c0b0497252
Shrawman <shrawmanrudra@gmail.com>
2024-06-16 17:36:27 +0600

    New file Added for testing

---
+++ hello.py
@@ -0,0 +1,8 @@
+friends = ['joya','nayon','sadia','mithi','anik']
+name = input ("Enter a name: ")
+
+for i in friends:
+    if (i == lower(name)):
+        print("You've found your friend", i)
+        break
+
