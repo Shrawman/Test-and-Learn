7f25f79a6be4757d48047d8ecb004d13c302437d
Shrawman <shrawmanrudra@gmail.com>
2024-06-18 16:41:23 +0600

    Iterator

---
+++ iterator.py
@@ -0,0 +1,29 @@
+#  s = ["hello", "my", "friend","how","are","you"]
+# itr = iter(s)
+# revitr= reversed(s)
+# print(next(itr))
+# print(next(revitr))  # cntrl + /
+
+#Remote Control
+
+class RemoteControl:
+    def __init__(self):
+        self.channel = ["HBO","Disney","Star Sports","Ten Sports"]
+        self.index = -1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.index += 1
+        if self.index == len(self.channel):
+            raise StopIteration
+        return self.channel[self.index]
+
+r = RemoteControl()
+itr = iter(r)
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
