8038d5f60b42fcdfabc293a0806bc4b1056eeee0
Shrawman <shrawmanrudra@gmail.com>
2024-06-18 17:36:29 +0600

    generator added

--- iterator.py
+++ iterator.py
@@ -6,24 +6,32 @@
 
 #Remote Control
 
-class RemoteControl:
-    def __init__(self):
-        self.channel = ["HBO","Disney","Star Sports","Ten Sports"]
-        self.index = -1
+# class RemoteControl:
+#     def __init__(self):
+#         self.channel = ["HBO","Disney","Star Sports","Ten Sports"]
+#         self.index = -1
 
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
     
-    def __next__(self):
-        self.index += 1
-        if self.index == len(self.channel):
-            raise StopIteration
-        return self.channel[self.index]
+#     def __next__(self):
+#         self.index += 1
+#         if self.index == len(self.channel):
+#             raise StopIteration
+#         return self.channel[self.index]
 
-r = RemoteControl()
-itr = iter(r)
-print(next(itr))
-print(next(itr))
-print(next(itr))
-print(next(itr))
-print(next(itr))
+# r = RemoteControl()
+# itr = iter(r)
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+
+def remote_control():
+    yield "cnn"
+    yield "espn"
+
+x = remote_control()
+print(next(x))
+print(next(x))
